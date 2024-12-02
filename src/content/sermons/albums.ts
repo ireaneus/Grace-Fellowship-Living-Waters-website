fc44e2add6bfb80a5d17e59d0e38d2399b48b6fa
ireaneus <bwdave@protonmail.com>
2024-12-02 14:59:29 -0700

    images

--- src/content/sermons/albums.ts
+++ src/content/sermons/albums.ts
@@ -9,13 +9,31 @@ export interface Album {
     {
       id: "1Corinthians",
       title: "1 Corinthians",
-      coverImage: "/images/albums/1corinthians.jpg",
+      coverImage: "/images/albums/openbible-read.jpg",
       description: "A study through First Corinthians"
     },
     {
-      id: "2TIM",
-      title: "2 Timothy",
-      coverImage: "/images/albums/2timothy.jpg",
-      description: "A study through Second Timothy"
-    }
+        id: "1TIM",
+        title: "1 Timothy",
+        coverImage: "/images/albums/openbible-glasses.jpg",
+        description: "A study through First Timothy"
+      },
+    {
+      id: "Acts",
+      title: "Book of Acts",
+      coverImage: "/images/albums/openbible-leaves.jpg",
+      description: "A study through the Book of Acts - Writer:Luke"
+    },
+    {
+        id: "BibleSurvey",
+        title: "Bible Survey",
+        coverImage: "/images/albums/bible-glory.jpg",
+        description: "A Bible Survey"
+      },
+      {
+        id: "Daniel",
+        title: "Book of Daniel",
+        coverImage: "/images/albums/revisedbible.jpg",
+        description: "A study through the Book of Daniel"
+      }
   ];
